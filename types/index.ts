baa459fbd0f31a53475fa1ef65aaa093d624a77c
Angelo Zambrano <angelozam17@gmail.com>
2021-06-07 10:01:36 -0500

    feat: firebase config

---
+++ types/index.ts
@@ -0,0 +1,12 @@
+export interface Good {
+  cost: number;
+  dealer: number;
+  help: number;
+  id: string;
+  image: string;
+  level: number;
+  name: string;
+  sell_price: number;
+  time: number;
+  xp: number;
+}
