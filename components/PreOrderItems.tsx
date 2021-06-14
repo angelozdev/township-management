c9a6cbda417528a7d1ac91e2cabe462165517ceb
Angelo Zambrano <angelozam17@gmail.com>
2021-06-14 11:03:50 -0500

    feat: add back button

--- components/PreOrderItems.tsx
+++ components/PreOrderItems.tsx
@@ -1,11 +1,12 @@
+import { Feed, Good } from "types";
 import { PreOrderItem } from "./";
 
-interface Props<T = any> {
-  items: Array<T>;
+interface Props {
+  items: Array<Feed | Good>;
   title: string;
 }
 
-function PreOrderItems<T>({ items, title }: Props<T>) {
+function PreOrderItems({ items, title }: Props) {
   if (!items.length) {
     return null;
   }
@@ -16,7 +17,7 @@ function PreOrderItems<T>({ items, title }: Props<T>) {
         <h2 className="text-xl font-semibold">{title}</h2>
       </div>
       <ul>
-        {items.map((good: any) => (
+        {items.map((good) => (
           <PreOrderItem key={good.id} name={good.name} />
         ))}
       </ul>
--- pages/preorder.tsx
+++ pages/preorder.tsx
@@ -1,14 +1,26 @@
 import { Layout, PreOrderItems, Wrapper } from "components";
 import { usePreOrderContext } from "context/preorder";
+import { useRouter } from "next/router";
 
 function PreOrder() {
   const { goods, feeds } = usePreOrderContext();
+  const router = useRouter();
 
   return (
     <Layout>
       <Wrapper size="lg">
-        <PreOrderItems items={goods} title="Goods" />
-        <PreOrderItems items={feeds} title="Feeds" />
+        <div className="mb-4">
+          <button
+            className="bg-indigo-800 text-white p-2 rounded-md"
+            onClick={() => router.back()}
+          >
+            <span className="mx-2">⬅️ Back</span>
+          </button>
+        </div>
+        <div className="p-4 border rounded-md">
+          <PreOrderItems items={goods} title="Goods" />
+          <PreOrderItems items={feeds} title="Feeds" />
+        </div>
       </Wrapper>
     </Layout>
   );
